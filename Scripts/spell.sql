67dadac4b109dd95504da7dfc4bc57240115ce86
Carlos Castañeda <carloscodvcf@gmail.com>
2025-01-23 17:57:05 +0100

    Eliminacion de scripts de levantamiento de tablas

--- Scripts/spell.sql
+++
@@ -1,9 +0,0 @@
--- script para levantar la tabla spell
-CREATE TABLE spell (
-    spellID INT AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    damage DOUBLE NOT NULL,
-    mana_needed_to_invoque DOUBLE NOT NULL,
-    PRIMARY KEY (spellID)
-);
